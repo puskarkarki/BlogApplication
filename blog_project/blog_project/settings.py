9cc67e79d75cc6e6f83bd3b81ff02142e0c37efa
puskarkarki <puskarrajkarki1234@gmail.com>
2021-09-03 16:04:05 +0545

    smtp server setting

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -79,12 +79,12 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com',
-EMAIL_HOST_USER = 'puskarrajkarki1234@gmail.com',
-EMAIL_HOST_PASSWORD = 'puskar123',
-EMAIL_PORT = 587,
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'puskarrajkarki1234@gmail.com'
+EMAIL_HOST_PASSWORD = 'cre@tepa$$word@9805'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 # Password validation
